f1ccbe101a554a811cd01225fb48247dfd78c0c6
majorazero <daniel.cw.hsu@gmail.com>
2018-09-23 21:29:54 -0700

    implemented department view

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const chalk = require("chalk");
 const Table = require("cli-table");
 
 let connection = mysql.createConnection({
@@ -38,6 +39,55 @@ function mainMenu(){
   });
 }
 
+function departmentPerform(){
+  connection.query("SELECT * FROM products RIGHT JOIN departments ON departments.department_name = products.department_name ORDER BY departments.department_id",function(err,res){
+    let table = new Table({
+      head: [chalk.hex("#719dba")("Department ID"),chalk.hex("#719dba")("Department Name"),chalk.hex("#719dba")("Over Head Costs($)"), chalk.hex("#719dba")("Product Sales($)"), chalk.hex("#719dba")("Total Profit($)")]
+    });
+    let currDepId;
+    let currDepInfo = {};
+    for(let i = 0; i < res.length; i++){
+      if (res[i].department_id !== currDepId){
+        if(currDepId !== undefined){
+          table.push([currDepId,
+                        currDepInfo.departmentName,
+                        currDepInfo.overHeadCost,
+                        currDepInfo.product_sales,
+                        currDepInfo.product_sales-currDepInfo.overHeadCost]);
+        }
+        //if the current dep id is not equal to the current department id, we set it to the new one
+        currDepId = res[i].department_id;
+        currDepInfo = {};
+        currDepInfo.overHeadCost = res[i].over_head_costs;
+        currDepInfo.departmentName = res[i].department_name;
+        if(res[i].product_sales === null){
+          currDepInfo.product_sales = 0;
+        }
+        else {
+          currDepInfo.product_sales = res[i].product_sales;
+        }
+        if(i === res.length-1){
+          table.push([currDepId,
+                        currDepInfo.departmentName,
+                        currDepInfo.overHeadCost,
+                        currDepInfo.product_sales,
+                        currDepInfo.product_sales-currDepInfo.overHeadCost]);
+        }
+      }
+      else {
+        if(res[i].product_sales === null){
+          currDepInfo.product_sales = 0;
+        }
+        else {
+          currDepInfo.product_sales += res[i].product_sales;
+        }
+      }
+    }
+  console.log(table.toString());
+  mainMenu();
+  });
+}
+
 function addDepartment(){
   inquirer.prompt({
     message: "What's the name of the new department?",
@@ -65,20 +115,5 @@ function addDepartment(){
         mainMenu();
       }
     });
-  });
-}
-
-function departmentPerform(){
-  connection.query("SELECT department_name FROM departments",function(err,response){
-    let table = new Table({
-      head: ["department_id","department_name","Product Sales","Total Profits","Over Head Costs"],
-      colWidths: [15,20,18,18,18]
-    });
-    console.log(response);
-
-    // for(let i = 0; i < response.length; i++){
-    //
-    // }
-    //console.log(table.toString());
   });
 }
